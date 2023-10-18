9c50206d3b3395ae0a8394debfa1a18f49daf98b
unknown <vjvijay130@gmail.com>
2023-10-18 18:24:54 +0530

    addemployee page starting

--- src/Pages/AddEmployee/AddEmployee.js
+++ src/Pages/AddEmployee/AddEmployee.js
@@ -6,49 +6,53 @@ import '../AddEmployee/AddEmployee.css';
 import { useNavigate } from "react-router-dom";
 
 export default function AddEmployee() {
-    const navigate = useNavigate();
-    const [profileData, setProfileData] = useState("");
+  const navigate = useNavigate();
+  const [profileData, setProfileData] = useState("");
 
-    useEffect(() => {
-        let ignore = false;
+  useEffect(() => {
+    let ignore = false;
 
-        if (!ignore) getProfiledata()
-        return () => { ignore = true; }
-    }, []);
+    if (!ignore) getProfiledata()
+    return () => { ignore = true; }
+  }, []);
 
-    async function getProfiledata() {
-        try {
-            const res = await fetch(
-                "https://localhost:44388/Authentication/ProfileData",
-                {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("JwtToken")}`
-                    },
-                }
-            )
-            const profileData = await res.json();
-            if (profileData.resCode === 200) {
-                console.log(profileData.resData);
-                setProfileData(profileData.resData);
-            }
-        } catch (e) {
-            console.log("ok");
-            navigate("/", { replace: true });
+  async function getProfiledata() {
+    try {
+      const res = await fetch(
+        "https://localhost:44388/Authentication/ProfileData",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("JwtToken")}`
+          },
         }
-        // const profileData = await res.json();
-        // if (profileData.resCode === 200) {
-        //   console.log(profileData.resData);
-        //   setProfileData(profileData.resData);
-        // }
+      )
+      const profileData = await res.json();
+      if (profileData.resCode === 200) {
+        console.log(profileData.resData);
+        setProfileData(profileData.resData);
+      }
+    } catch (e) {
+      console.log("ok");
+      navigate("/", { replace: true });
     }
+    // const profileData = await res.json();
+    // if (profileData.resCode === 200) {
+    //   console.log(profileData.resData);
+    //   setProfileData(profileData.resData);
+    // }
+  }
+
+  const NavBack = () => {
+    navigate("/HR", { replace: true });
+  }
 
 
-    return (
-        <div>
-            <AppHeader data={profileData} />
+  return (
+    <div>
+      <AppHeader data={profileData} />
 
-            <div class="breadcrumb-area">
+      <div class="breadcrumb-area">
         <div class="container-fluid">
           <div class="row pt-1 pb-1">
             <div class="col-md-6">
@@ -66,7 +70,89 @@ export default function AddEmployee() {
         </div>
       </div>
 
+      <div className='containner p-2' style={{ height: "600px", overflow: "auto", backgroundColor: "#f3f5f9" }} >
 
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="bg-boxshadow">
+              <div class="ibox-content">
+
+                <div class="col-lg-12">
+                  <div class="form-group d-flex">
+                    <span class="pull-right">
+                      <button class="FunctionButton" style={{ backgroundColor: "#183985", color: "white" }} onClick={NavBack}>Back</button>
+
+                    </span>
+                  </div>
+                </div>
+
+                <div class="col-lg-12">
+                  <div class="form-group d-flex">
+                    <h4>Professional Details:-</h4>
+                  </div>
+                </div>
+
+                <div class="col-lg-12">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="form-group d-flex">
+                      <label class="col-md-5 mt-1">Name <span style={{ color: "red" }}>*</span> <span class="float-right">:</span></label>
+                      <div class="col-md-7">
+                        <input
+                          style={{ width: "100%" }}
+                          type='text'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="form-group d-flex">
+                      <label class="col-md-5 mt-1">TeamType <span style={{ color: "red" }}>*</span> <span class="float-right">:</span></label>
+                      <div class="col-md-7">
+                        <select
+                            style={{width: "100%"}}
+                        >
+                          <option value={"null"}>Select</option>
+                          <option value={"Sales Team"}>Sales Team</option>
+                          <option value={"Others"}>Others</option>
+                        </select>
+                      </div>
+                    </div>
+                </div>
+                  <div class="col-lg-6 mt-1">
+                    <div class="form-group d-flex">
+                      <label class="col-md-5 mt-1">Hierarchy<span style={{ color: "red" }}>*</span> <span class="float-right">:</span></label>
+                      <div class="col-md-7">
+                        <input
+                          style={{ width: "100%" }}
+                          type='text'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mt-1">
+                    <div class="form-group d-flex">
+                      <label class="col-md-5 mt-1">Reporting To<span style={{ color: "red" }}>*</span> <span class="float-right">:</span></label>
+                      <div class="col-md-7">
+                        <select
+                            style={{width: "100%"}}
+                        >
+                          <option value={"null"}>Select</option>
+                          <option value={"Sales Team"}>Sales Team</option>
+                          <option value={"Others"}>Others</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+
+    </div>
+  )
 }
